5a75ee39c5481e83373548365cd3f21c0c407bda
Lucas Brentano <brentano.l@gmail.com>
2022-07-20 11:16:44 -0300

    Initial commit

---
+++ DuckHunt3D/Level/Level.gd
@@ -0,0 +1,25 @@
+extends Spatial
+
+onready var gun_pivot := $GunPivot
+onready var gun := $GunPivot/Gun
+onready var camera := $Camera
+
+var _ray_start_position := Vector3()
+var _ray_target_position := Vector3()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var _mouse_position: Vector2 = get_viewport().get_mouse_position()
+		
+		_ray_start_position = camera.project_ray_origin(_mouse_position)
+		
+		_ray_target_position = _ray_start_position + camera.project_ray_normal(_mouse_position) * 200
+		
+		var _space_state := get_world().direct_space_state
+		var _intersection := _space_state.intersect_ray(_ray_start_position, _ray_target_position)
+		
+		if not _intersection.empty():
+			var _position = _intersection.position
+			gun_pivot.look_at(_position, Vector3.UP)
+		
+		
