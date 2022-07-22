8b5f7bdb788ec63fd9381519b558bf0568bf3d8c
Lucas Brentano <brentano.l@gmail.com>
2022-07-22 17:54:15 -0300

    Point Made Animation

--- DuckHunt3D/Gui/GUI.gd
+++ DuckHunt3D/Gui/GUI.gd
@@ -3,3 +3,11 @@ extends Control
 
 func change_score(new_score: int) -> void:
 	$CenterContainer/ScoreLabel.text = "Score: %s" %new_score
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		$PointMade.rect_position = get_global_mouse_position()
+		
+	if event.is_action_pressed("ui_down"):
+		$AnimationPlayer.play("PointMadeAnimation")
