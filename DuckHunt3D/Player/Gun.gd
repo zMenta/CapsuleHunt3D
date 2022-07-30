9cded4ed7e89114dec32cce3c15157cc580bf439
Lucas Brentano <brentano.l@gmail.com>
2022-07-29 21:52:00 -0300

    Basic Ammo

--- DuckHunt3D/Player/Gun.gd
+++ DuckHunt3D/Player/Gun.gd
@@ -2,14 +2,22 @@ extends Spatial
 
 onready var ray := $RayCast
 
+var ammo = 2
+var busy = false
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("fire"):
-		fire()
+		if ammo > 0:
+			print("fired")
+			fire()
+			ammo -= 1
+		else:
+			reload()
+		
 	
 	if event.is_action_pressed("reload"):
-		$AnimationPlayer.play("Reload")
-
+		reload()
 
 func fire() -> void:
 	var collider : Object = ray.get_collider()
@@ -22,4 +30,11 @@ func fire() -> void:
 			
 func eject_shells() -> void:
 	$ShellParticles.emitting = true
-	
+
+func reload() -> void:
+	$AnimationPlayer.play("Reload")
+	ammo = 2
+	busy = true
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	busy = false
