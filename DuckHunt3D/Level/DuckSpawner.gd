3f8a039ae4665e7da6575f01e81d7a7a73979a81
Lucas Brentano <brentano.l@gmail.com>
2022-08-05 19:45:32 -0300

    Different Duck Spawning

--- DuckHunt3D/Level/DuckSpawner.gd
+++ DuckHunt3D/Level/DuckSpawner.gd
@@ -3,19 +3,27 @@ extends Spatial
 signal duck_spawned(duck)
 
 export(PackedScene) var Duck
+export(PackedScene) var TimeDuck
+export var duck_counter_threshold := 5
 
 onready var path_follow := $DuckPath/PathFollow
 
+var _duck_counter := 0
 
 func _on_SpawnTime_timeout() -> void:
 	path_follow.offset = randi()
+	var duck_instance: Spatial = null
 	
-	var duck_instance = Duck.instance() as Spatial
+	if _duck_counter < duck_counter_threshold:
+		duck_instance = Duck.instance()
+		_duck_counter += 1
+	else:
+		_duck_counter = 0
+		duck_instance = TimeDuck.instance()
+		
 	duck_instance.global_transform = path_follow.global_transform
 	duck_instance.rotation_degrees += Vector3(0,90,0)
 	
 	emit_signal("duck_spawned", duck_instance)
 	get_tree().current_scene.add_child(duck_instance)
-	
-	
 	
