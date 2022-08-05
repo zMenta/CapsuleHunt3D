478921b5d8d4ddd4cc99c17821fa9682285baea7
Lucas Brentano <brentano.l@gmail.com>
2022-08-05 20:40:16 -0300

    TimeDuck Kill BugFix

--- DuckHunt3D/Main/Main.gd
+++ DuckHunt3D/Main/Main.gd
@@ -20,8 +20,8 @@ func _on_Duck_died(duck_body: KinematicBody) -> void:
 		$GUI.point_made_animation()
 		current_score += 100
 		$GUI.change_score(current_score)
-	if duck_body.is_in_group("time_duck"):
-		$GameTimer.start($GameTimer.time_left + 4.0)
+		if duck_body.is_in_group("time_duck"):
+			$GameTimer.start($GameTimer.time_left + 4.0)
 
 
 func _on_GameTimer_timeout():
